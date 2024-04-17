b668e22e9119569a47a1ed1c237cac342a43ae67
Rahul <Rahul>
2024-04-17 18:10:56 +0530

    Modified app.py file

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/test')
+def hello():
+    return 'Hello, this is test website'
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", debug=True)
+    app.run(host="127.0.0.1", debug=True)
